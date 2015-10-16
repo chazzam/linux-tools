c80774773a083e9fdfbf476e98ba3739108cc4df
chazzam <chazz.m@gmail.com>
2015-10-16 11:11:26 -0500

    updating commentary to potentially be easier to reproduce elsewhere
    adding some further information about why some steps where taken,
    and where some fields came from. hope it helps!

--- displaylink_add.sh
+++ displaylink_add.sh
@@ -2,10 +2,11 @@
 
 #systemctl start dislplaylink
 
+# Handle name changing of LVDS, and we base arrangement on the DP-1-1 screen
 LVDS="$(xrandr |grep LVDS|grep \ connected|cut -d\  -f1)"
 DP1="$(xrandr |grep DP-1-1|grep \ connected|cut -d\  -f1)"
 
-
+# make sure the device is connected, and then more "magic"
 UDL="$(xrandr --listproviders |grep -c modesetting)";
 [ "$UDL" = "1" ] || ( echo "more than one UDL device"; exit 1; );
 xrandr --setprovideroutputsource modesetting Intel
@@ -13,10 +14,13 @@ xrandr --setprovideroutputsource modesetting Intel
 DVI="$(xrandr |grep DVI|cut -d\  -f1)";
 [ "$DVI" != "" ] || ( echo "No DVI device detected"; exit 1; );
 
-# works on an e1649Fwu DisplayLink USB display
+# works on an e1649Fwu DisplayLink USB display, YMMV.
+# Recommend looking up correct settings for your display
+# or try `gtf` or `cvt` to generate the modeline
 xrandr --newmode "1368x768_72.00"  104.73  1368 1448 1592 1816  768 769 772 801  -HSync +Vsync
 xrandr --addmode $DVI 1368x768_72.00
 
+# Handle screen layout
 # If not in the dock, then add the displaylink to the left of the screen
 if [ "$DP1" != "" ]; then
   xrandr --output $DVI --below $DP1 --mode 1368x768_72.00
--- dock_monitors.sh
+++ dock_monitors.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# get the nvidia and intel chips
+# make sure both gpus are still available
+# Did you forge to load a module!?
 NV="$(xrandr --listproviders |grep -c nouveau)";
 INTEL="$(xrandr --listproviders |grep -c Intel)";
 [ "$NV" != "" ] || ( echo "Nouveau device not found"; exit 1; );
 [ "$INTEL" != "" ] || ( echo "Intel device not found"; exit 1; );
 
 # requires RandR 1.4 or later
+# This is the "magic" that makes Nvidia Optimus work
 xrandr --setprovideroffloadsink nouveau Intel;
 xrandr --setprovideroutputsource nouveau Intel
 
@@ -17,8 +19,12 @@ DP2="$(xrandr |grep DP-1-2|grep \ connected|cut -d\  -f1)"
 [ "$DP1" != "" ] || ( echo "DP-1-1 not found"; exit 1; );
 [ "$DP2" != "" ] || ( echo "DP-1-2 not found"; exit 1; );
 
+# Kept having issues where the name of this would change
+# Sometimes its LVDS1, sometimes LVDS2
+# so now we just get the connected LVDS and base around that
 LVDS="$(xrandr |grep LVDS|grep \ connected|cut -d\  -f1)";
 
+# configure monitor layout!
 xrandr --output $DP1 --auto --left-of $LVDS
 xrandr --output $DP2 --auto --right-of $LVDS
 xrandr --output $LVDS --primary
