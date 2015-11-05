a3c64cab2fceab925541ceb151deb1a953b412be
chazzam <chazz.m@gmail.com>
2015-11-05 10:05:47 -0600

    fixing displaylink
    added sudo to systemctl command because the popup appears to be run
    in a background process, causing the rest of setup to always fail...

--- displaylink_add.sh
+++ displaylink_add.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#systemctl start dislplaylink
+sudo systemctl start displaylink
+sleep 4
 
 # Handle name changing of LVDS, and we base arrangement on the DP-1-1 screen
 LVDS="$(xrandr |grep LVDS|grep \ connected|cut -d\  -f1)"
@@ -8,11 +9,11 @@ DP1="$(xrandr |grep DP-1-1|grep \ connected|cut -d\  -f1)"
 
 # make sure the device is connected, and then more "magic"
 UDL="$(xrandr --listproviders |grep -c modesetting)";
-[ "$UDL" = "1" ] || ( echo "more than one UDL device"; exit 1; );
+[ "$UDL" = "1" ] || ( echo "more than one UDL device"; false; ) || exit 1;
 xrandr --setprovideroutputsource modesetting Intel
 
 DVI="$(xrandr |grep DVI|cut -d\  -f1)";
-[ "$DVI" != "" ] || ( echo "No DVI device detected"; exit 1; );
+[ "$DVI" != "" ] || ( echo "No DVI device detected"; false; ) || exit 1;
 
 # works on an e1649Fwu DisplayLink USB display, YMMV.
 # Recommend looking up correct settings for your display
